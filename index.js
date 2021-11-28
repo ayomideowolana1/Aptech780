9d63046fa316c8e5f817c4a569c25333efc59990
Ayomide Owolana <ayomideowolana1@gmail.com>
2021-11-28 16:25:06 +0100

    topfive finishes added, scores updated

--- index.js
+++ index.js
@@ -2,92 +2,110 @@ const competitors =[
     {
         name: "Daniel",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive: 0,
     },
     {
         name: "Ahmed",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive: 0,
     },
     {
         name: "Clinton",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive: 0,
     },
     {
         name: "Eleazar",
         points : 5,
-        targetsMet: 1
+        targetsMet: 1,
+        topFive: 1,
     },
     // {
-    //     name: "Jade",
-    //     points : 0,
-    //     targetsMet: 0
-    // },
-    {
-        name: "Juwon",
-        points : 0,
-        targetsMet: 0
-    },
-    {
-        name: "Samuel",
-        points : 0,
-        targetsMet: 0
-    },
-    {
-        name: "Victor",
-        points : 0,
-        targetsMet: 0
-    },
-    {
-        name: "Success",
-        points : 0,
-        targetsMet: 0
+        //     name: "Jade",
+        //     points : 0,
+        //     targetsMet: 0,
+        // topFive: 0,
+        // },
+        {
+            name: "Juwon",
+            points : 5,
+            targetsMet: 1,
+            topFive: 1,
+        },
+        {
+            name: "Samuel",
+            points : 0,
+            targetsMet: 0,
+            topFive: 0,
+        },
+        {
+            name: "Victor",
+            points : 5,
+            targetsMet: 1,
+            topFive: 1,
+        },
+        {
+            name: "Success",
+            points : 0,
+            targetsMet: 0,
+            topFive: 0,
     },
     {
         name: "Prosper",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
     {
         name: "Joshua",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
     {
         name: "Nyerho",
         points : 5,
-        targetsMet: 1
+        targetsMet: 1,
+        topFive:1,
     },
     {
         name: "Lateef",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
     {
         name: "Zarouk",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
     {
         name: "Esther",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
     {
         name: "Wofe",
         points : 5,
-        targetsMet: 1
+        targetsMet: 1,
+        topFive:1,
     },
     {
         name: "Kobi",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
     {
         name: "David",
         points : 0,
-        targetsMet: 0
+        targetsMet: 0,
+        topFive:0,
     },
 ]
 
@@ -115,7 +133,8 @@ const row = document.createElement("tr");
         <td>${i+1}</td>
         <td style="text-align: center;">${competitors[i].name}</td>
         <td style="text-align: center;">${competitors[i].points}</td>
-        <td style="text-align: center;">${competitors[i].targetsMet}</td>`
+        <td style="text-align: center;">${competitors[i].targetsMet}</td>
+        <td style="text-align: center;">${competitors[i].topFive}</td>`
 
         table.append(row.cloneNode(true))
     }
